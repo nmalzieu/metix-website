331303c917959d774dc24c49bd3123525c34a01b
nmalzieu <noe.malzieu@gmail.com>
2014-06-23 14:30:03 +0200

    Corrected contact@

--- post.php
+++ post.php
@@ -9,7 +9,7 @@ if (isset($_POST['name']) && $_POST['name'] != "" && isset($_POST['email']) && $
         curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v2/metix.io/messages');
         curl_setopt($ch, CURLOPT_POSTFIELDS, array(
             'from' => $_POST['email'],
-            'to' => 'Metix <noe@metix.io>',
+            'to' => 'Metix <contact@metix.io>',
             'subject' => 'Contact from '.$_POST['name'],
             'text' => $_POST['message']
             )
